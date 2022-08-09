5bc8246f76cc3bf0ca93e77d6e5abe86a32aa0cf
olypchuk <eragmont@gmail.com>
2022-08-09 11:23:27 +0300

    test deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App } from 'components/App';
+// import {App } from 'components/App';
 import './index.css';
 import './styles.css'
-
+import { useRef } from "react";
+const App = () => {
+  return <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />;
+};
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
@@ -11,4 +14,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
    </React.StrictMode>
 );
+
+
+
+const Player = ({ source }) => {
+  const playerRef = useRef();
+  const play = () => playerRef.current.play();
+  const pause = () => playerRef.current.pause();
+
+  return (
+    <div>
+      <video ref={playerRef} src={source}>
+        Sorry, your browser does not support embedded videos.
+      </video>
+      <div>
+        <button onClick={play}>Play</button>
+        <button onClick={pause}>Pause</button>
+      </div>
+    </div>
+  );
+};
 
